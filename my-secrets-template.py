5d26173b5c9014f2c55f8f8b29a1bf8f4f0c94ac
bobthechemist <bobthechemist@gmail.com>
2024-11-28 07:13:02 -0500

    Update and rename secrets-template.py to my-secrets-template.py
    added Adafruit_IO integration

---
+++ my-secrets-template.py
@@ -0,0 +1,6 @@
+secrets = {
+  'username': 'user@somewhere.com',
+  'password': 'password123',
+  'aio_user': 'Adafruit_IO username',
+  'aio_key' : 'Adafruit_IO key',
+}
--- secrets-template.py
+++
@@ -1,4 +0,0 @@
-secrets = {
-  'username': 'user@somewhere.com',
-  'password': 'password123'
-}
